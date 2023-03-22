ad9ec64cdd2947b389f9d20b6a0f6a14087d5e6c
Kamila Maciąg <kamila.maciag22@gmail.com>
2023-03-22 21:26:42 +0100

    created currencies.js

---
+++ src/App/currencies.js
@@ -0,0 +1,23 @@
+export const currencies = [
+    {
+        shortName: "EUR",
+        name: "EUR - euro",
+        rate: 4.67,
+    },
+    {
+        shortName: "USD",
+        name: "USD - dolar amerykański",
+        rate: 4.39,
+    },
+    {
+        shortName: "CHF",
+        name: "CHF - frank szwajcarski",
+        rate: 4.72,
+    },
+    {
+        shortName: "GBP",
+        name: "GBP - funt szterling",
+        rate: 5.3,
+    },
+
+];
