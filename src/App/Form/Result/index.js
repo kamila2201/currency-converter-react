6293970317323ad6aca88b7c311a6202ae90678e
Kamila Maciąg <kamila.maciag22@gmail.com>
2023-03-25 22:54:06 +0100

    added new className and styles for it

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
 
 export const Result = ({ result }) => (
-    <p>Otrzymasz: 
+    <p className="result">Otrzymasz:  
         {result !== "" &&
-            <strong className="result">{result.finalAmount.toFixed(2)} &nbsp; {result.currency}</strong>
+            <strong className="result__text">{result.finalAmount.toFixed(2)}&nbsp;{result.currency}</strong>
         }
     </p>
 );
