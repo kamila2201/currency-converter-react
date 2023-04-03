8898e3815d4707a5afb9c5e373b0cf2f73df8ab3
Kamila Maciąg <kamila.maciag22@gmail.com>
2023-04-03 11:46:37 +0200

    unnecessary spaces removed

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 export const Result = ({ result }) => (
-    <p className="result">Otrzymasz:  
+    <p className="result">Otrzymasz: 
         {result !== "" &&
             <strong className="result__text">{result.finalAmount.toFixed(2)}&nbsp;{result.currency}</strong>
         }
