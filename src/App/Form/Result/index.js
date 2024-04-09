8ad8953e8dc7a56760572f296a7114b90b331ed5
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-09 13:49:23 +0200

    showing final amount

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,7 +1,14 @@
 import "./style.css";
 
-const Result = () => (
-    <p className="result"></p>
+const Result = ({ result }) => (
+    !!result && (
+        <p className="result">
+            You will receive
+            <strong className="result__text">
+                {result.finalAmount.toFixed(2)}&nbsp;{result.currency}
+            </strong>
+        </p>
+    )
 )
 
 export default Result;
