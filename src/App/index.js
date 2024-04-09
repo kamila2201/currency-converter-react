6edd8458876bb0cc94446852291cb3146d380b7b
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-09 13:47:43 +0200

    calculateResult function

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,26 @@
 import './App.css';
+import {useState} from 'react'; 
+import { currencies } from './currencies';
 import Container from './Container';
 import Header from './Header';
 import Form from './Form';
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies.find(({shortName})=> shortName === currency).rate;
+
+    setResult({
+      finalAmount: +amount / rate,
+      currency,
+    })
+  }
+
   return (
     <Container>
       <Header />
-      <Form />
+      <Form result={result} calculateResult={calculateResult} />
     </Container>
   );
 }
