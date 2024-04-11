ec7b39b843d449ef8c5fd1789e75a2db3c3aee6c
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-11 16:48:54 +0200

    unused className="header" removed

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 const Header = () => (
-    <header className="header">
+    <header>
             <h1 className="header__title">Check how much money you will receive</h1>
         </header>
 )
