09a67b4d75a936c9da040cad6e408597afb5e853
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-13 19:06:50 +0200

    code formatted

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,11 +1,20 @@
-import {StyledForm, Legend, Fieldset, LabelText, Input, End, Button, Select} from "./styled"
-import {useState} from 'react';
+import { useState } from 'react';
 import Footer from "./Footer";
 import Result from "./Result";
 import { currencies } from "../currencies";
+import {
+    StyledForm,
+    Legend,
+    Fieldset,
+    LabelText,
+    Input,
+    End,
+    Button,
+    Select,
+} from "./styled";
 
 
-const Form = ({result, calculateResult}) => {
+const Form = ({ result, calculateResult }) => {
     const [amount, setAmount] = useState("");
     const [currency, setCurrency] = useState(currencies[0].shortName)
 
@@ -27,24 +36,24 @@ const Form = ({result, calculateResult}) => {
                     <label className="form__label">
                         <LabelText>Amount in PLN*:</LabelText>
                         <Input
-                        value={amount}
-                        onChange={({target})=> setAmount(target.value)} 
-                        type="number" 
-                        min="1" 
-                        step="0.01" 
-                        name="amount"
-                        placeholder="Enter the amount in PLN" 
-                        required
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            type="number"
+                            min="1"
+                            step="0.01"
+                            name="amount"
+                            placeholder="Enter the amount in PLN"
+                            required
                         />
                     </label>
                 </p>
                 <p>
                     <label>
                         <LabelText>Currency:</LabelText>
-                        <Select 
-                        value={currency}
-                        onChange={({target})=> setCurrency(target.value)} 
-                        name="currency">
+                        <Select
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                            name="currency">
                             {currencies.map(currency => (
                                 <option key={currency.shortName} value={currency.shortName}>
                                     {currency.name}
@@ -56,14 +65,14 @@ const Form = ({result, calculateResult}) => {
             </Fieldset>
             <End>
                 <Button>Convert</Button>
-                <Button 
-                onClick={onReset}
-                type="reset"
+                <Button
+                    onClick={onReset}
+                    type="reset"
                 >
                     Delete
                 </Button>
             </End>
-            <Result result={result}/>
+            <Result result={result} />
             <Footer />
         </StyledForm>
     )
