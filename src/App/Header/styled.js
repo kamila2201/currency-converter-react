9b83aa0808256e320438f8d83241cd52238c00c8
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-14 13:57:40 +0200

    added media queries for the Title

--- src/App/Header/styled.js
+++ src/App/Header/styled.js
@@ -3,5 +3,9 @@ import styled from "styled-components";
 export const Title = styled.h1`
     text-align: center;
     font-size: 40px;
-    color: ${({theme}) => theme.color.seaGreen};
+    color: ${({ theme }) => theme.color.seaGreen};
+
+    @media (max-width: ${({theme})=> theme.breakpoint.mobileMax}px){
+        font-size: 30px;
+    }
 `;
