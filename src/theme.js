ec480018899ec483fd39dcfdde547208e619569b
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-14 13:18:06 +0200

    added froly color to the theme

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,7 @@ export const theme = {
         white: "#fff",
         seaGreen: "#296e54",
         mandysPink: "#f2b7b7",
+        froly: "#f68585",
         gunPowder: "#42445a",
         semiTransparent: "#ffffffd3",
     },
