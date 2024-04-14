871f4f0318d2998c386dbe227b7238607db9ee72
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-14 13:09:17 +0200

    created theme.js withthe colors used on page

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    color: {
+        black: "#000",
+        white: "#fff",
+        seaGreen: "#296e54",
+        mandysPink: "#f2b7b7",
+        gunPowder: "#42445a",
+        semiTransparent: "#ffffffd3",
+    },
+};
