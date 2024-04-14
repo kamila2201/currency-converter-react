8ae0c7c2c22fe46d74b3fc5067a5d838b7e3ac96
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-14 13:24:08 +0200

    added transition

--- src/App/Form/Footer/styled.js
+++ src/App/Form/Footer/styled.js
@@ -11,6 +11,7 @@ export const Link = styled.a`
     margin-left: 5px;
     color: ${({theme}) => theme.color.seaGreen};
     text-decoration: none;
+    transition: color .3s;
 
     &:hover {
         color: ${({theme}) => theme.color.froly};
