4798464a716740dc8fb26512bd8c481bad1bca36
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-14 13:59:33 +0200

    removed webikt-box-shadow and moz-box-shadow

--- src/App/Container/styled.js
+++ src/App/Container/styled.js
@@ -6,7 +6,5 @@ export const StyledContainer = styled.main`
     margin: 50px auto;
     background-color: ${({ theme }) => theme.color.semiTransparent};
     border-radius: 20px;
-    -webkit-box-shadow: ${({ theme }) => theme.color.gunPowder};
-    -moz-box-shadow: 0px 0px 20px 21px ${({ theme }) => theme.color.gunPowder};
     box-shadow: 0px 0px 20px 21px ${({ theme }) => theme.color.gunPowder};
 `;
