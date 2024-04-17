44d0c90feac38f0e547ebe6df9e286d169c43de6
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-17 19:17:40 +0200

    added new styles

--- src/App/Form/Result/styled.js
+++ src/App/Form/Result/styled.js
@@ -1,10 +1,18 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.p`
+export const Wrapper = styled.div`
+    min-height: 70px;
+    padding:20px;
+    margin:10px;
     text-align: center;
 `;
 
-export const Text = styled.strong`
+export const ResultText = styled.strong`
     padding-left: 5px;
-    color: ${({theme}) => theme.color.seaGreen};
+    color: ${({ theme }) => theme.color.seaGreen};
+`;
+
+export const Text = styled.p`
+    margin:0;
+    padding: 0;
 `;
